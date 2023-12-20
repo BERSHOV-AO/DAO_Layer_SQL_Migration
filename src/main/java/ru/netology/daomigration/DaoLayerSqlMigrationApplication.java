2a37c9d278392fe1fda376ec3b17d09a6d1e9b95
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-12-20 13:58:09 +0300

    Initial commit

---
+++ src/main/java/ru/netology/daomigration/DaoLayerSqlMigrationApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.daomigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoLayerSqlMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaoLayerSqlMigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/netology/daomigration/DaoLayerSqlMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.daomigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoLayerSqlMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
